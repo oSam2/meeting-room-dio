460b895e8ef2f414797c66ac8d4c52ef8aff2354
oSam2 <samuelmarques96@live.com>
2021-09-08 18:49:13 -0300

    Initial commit

---
+++ Backend/src/main/java/one/crud/saladereuniao/saladereuniao/repository/RoomRepository.java
@@ -0,0 +1,12 @@
+package one.crud.saladereuniao.saladereuniao.repository;
+
+import one.crud.saladereuniao.saladereuniao.model.Room;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RoomRepository extends JpaRepository<Room, Long>{
+
+}
